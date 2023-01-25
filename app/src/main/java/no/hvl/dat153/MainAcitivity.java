3c171ab93d594917d3bbed8d459b14b0cdd184a1
Bjørn Eide <h595855@stud.hvl.no>
2023-01-25 14:08:25 +0100

    Reverted back to Original

--- app/src/main/java/no/hvl/dat153/MainAcitivity.java
+++ app/src/main/java/no/hvl/dat153/MainAcitivity.java
@@ -2,13 +2,20 @@ package no.hvl.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainAcitivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
