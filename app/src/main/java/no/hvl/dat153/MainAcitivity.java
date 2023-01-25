c851df5d1dbfa49cd5bccb7b8ab21246c4f6fb32
Bjørn Eide <h595855@stud.hvl.no>
2023-01-25 14:03:30 +0100

    Added a button with text hello.

--- app/src/main/java/no/hvl/dat153/MainAcitivity.java
+++ app/src/main/java/no/hvl/dat153/MainAcitivity.java
@@ -2,13 +2,31 @@ package no.hvl.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainAcitivity extends AppCompatActivity {
 
+
+    private Button hello;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = (Button) findViewById(R.id.Hello);
+
+        hello.setOnClickListener((View.OnClickListener) this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text = "Test";
+        Toast.makeText(MainAcitivity.this, text, Toast.LENGTH_SHORT).show();
     }
 }
