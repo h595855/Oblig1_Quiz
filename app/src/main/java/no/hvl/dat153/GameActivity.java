ee64bdda7904fc82a62939919c3d0dac6fd84b37
Bjørn Eide <h595855@stud.hvl.no>
2023-01-30 14:36:12 +0100

    Fikset bilde visning.

--- app/src/main/java/no/hvl/dat153/GameActivity.java
+++ app/src/main/java/no/hvl/dat153/GameActivity.java
@@ -2,7 +2,14 @@ package no.hvl.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        TypedArray list = getResources().obtainTypedArray(R.array.images);
+        for (int i = 0; i < list.length(); ++i) {
+            int id = list.getResourceId(i, -1);
+        }
+
+        View id = (ImageView) findViewById(R.id.image);
+
     }
+
 }
--- app/src/main/java/no/hvl/dat153/MainAcitivity.java
+++ app/src/main/java/no/hvl/dat153/MainAcitivity.java
@@ -1,8 +1,12 @@
 package no.hvl.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
+import no.hvl.dat153.MainAcitivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainAcitivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainAcitivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.Start);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainAcitivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
