f7585c5406dd79cd4909dda00537c8b4c6f20ffa
Maksim <589664@stud.hvl.no>
2023-02-09 14:18:29 +0100

    working selector

--- app/src/main/java/no/hvl/dat153/AddPictureActivity.java
+++ app/src/main/java/no/hvl/dat153/AddPictureActivity.java
@@ -1,49 +1,97 @@
 package no.hvl.dat153;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 
 public class AddPictureActivity extends AppCompatActivity {
 
+    //preview of the selected image
     ImageView imageView;
+
+    //upload button
+    Button uploadButton;
+
+    // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_picture);
 
+        // register the UI widgets with their appropriate IDs
+        imageView = findViewById(R.id.imageView);
+        uploadButton = findViewById(R.id.uploadButton);
 
-    }
-
-    private void selectImage() {
-
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri uri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                // set the selected image to image view
-                imageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
+        /*
+        handle the Choose Image button to trigger
+        the image chooser function
+         */
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePicker();
             }
-        }
+        });
     }
 
+    private void imagePicker()
+    {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
 
-}
+        launchSomeActivity.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> launchSomeActivity
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // do your operation from here....
+                    if (data != null
+                            && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        Bitmap selectedImageBitmap = null;
+                        try {
+                            selectedImageBitmap
+                                    = MediaStore.Images.Media.getBitmap(
+                                    this.getContentResolver(),
+                                    selectedImageUri);
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        imageView.setImageBitmap(
+                                selectedImageBitmap);
+                    }
+                }
+            });
+
+
+
+}//class
+
+
+
+
+
